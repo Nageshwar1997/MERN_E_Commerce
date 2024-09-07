43d8fc946a1e7e460a47aa78f5ea3c8b29102e13
Nageshwar <nageshpawarpatil@gmail.com>
2024-09-07 15:48:35 +0530

    Frontend Almost Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,5 @@
 import "./App.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-
-import Footer from "./customer/components/footer/Footer";
-import Navigation from "./customer/components/navigation/Navigation";
-// import HomePage from "./customer/pages/HomePage/HomePage";
-// import Product from "./customer/components/product/Product";
-// import ProductDetails from "./customer/components/productDetails/ProductDetails";
-// import Cart from "./customer/components/cart/Cart"
-// import Checkout from "./customer/components/checkout/Checkout";
-import Order from "./customer/components/order/Order";
-import OrderDetails from "./customer/components/order/OrderDetails";
 import CustomerRoutes from "./customer/routes/CustomerRoutes";
 
 function App() {
