3850ab8cfd0bdd0d4f85a20600f3feddcecb9555
Nageshwar <nageshpawarpatil@gmail.com>
2024-12-22 20:33:48 +0530

    Created a Product Section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,14 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Footer from "./customer/components/Footer/Footer";
 import Navigation from "./customer/components/Navigation/Navigation";
 import HomePage from "./customer/pages/HomePage/HomePage";
+import Product from "./customer/components/Product/Product";
 
 function App() {
   return (
     <Router>
       <Navigation />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Product />} />
       </Routes>
       <Footer />
     </Router>
