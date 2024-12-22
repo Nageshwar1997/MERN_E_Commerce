9329db34d069574024e99578f5c0903eb08b5d00
Nageshwar <nageshpawarpatil@gmail.com>
2024-12-22 17:49:29 +0530

    temp routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Footer from "./customer/components/Footer/Footer";
 import Navigation from "./customer/components/Navigation/Navigation";
 import HomePage from "./customer/pages/HomePage/HomePage";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navigation />
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
