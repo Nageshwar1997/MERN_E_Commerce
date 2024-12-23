311ce43fdedbba460fcde7bb7e6aec62d2abfc6a
nageshwarp1997 <nageshwar@ctruh.com>
2024-12-23 17:30:22 +0530

    database connected with mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 const app = require(".");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5454;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  await connectDB();
   console.log(`E-commerce app listening on port: ${PORT}`);
 });
