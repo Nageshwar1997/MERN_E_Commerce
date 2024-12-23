0f0d70f617472c8573b0911fb2ff03d348e1da1a
nageshwarp1997 <nageshwar@ctruh.com>
2024-12-23 16:32:47 +0530

    Created a Customer Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,17 @@
 import "./App.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Footer from "./customer/components/Footer/Footer";
-import Navigation from "./customer/components/Navigation/Navigation";
-import HomePage from "./customer/pages/HomePage/HomePage";
-import Product from "./customer/components/Product/Product";
-import ProductDetails from "./customer/components/ProductDetails/ProductDetails";
-import Cart from "./customer/components/cart/Cart";
-import Checkout from "./customer/components/checkout/Checkout";
-import Order from "./customer/components/order/Order";
-import OrderDetails from "./customer/components/order/OrderDetails";
+
+import CustomerRoutes from "./routes/CustomerRoutes";
 
 function App() {
   return (
-    <Router>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<OrderDetails />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<CustomerRoutes />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
---
+++ frontend/src/routes/CustomerRoutes.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import HomePage from "../customer/pages/HomePage/HomePage";
+import Cart from "../customer/components/cart/Cart";
+import Navigation from "../customer/components/Navigation/Navigation";
+import Footer from "../customer/components/Footer/Footer";
+import Product from "../customer/components/Product/Product";
+import ProductDetails from "../customer/components/ProductDetails/ProductDetails";
+import Checkout from "../customer/components/checkout/Checkout";
+import Order from "../customer/components/order/Order";
+import OrderDetails from "../customer/components/order/OrderDetails";
+
+const CustomerRoutes = () => {
+  const location = useLocation();
+
+  console.log("location", location);
+  console.log("location.pathname", location.pathname);
+  return (
+    <div>
+      <div className="">
+        <Navigation />
+      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/:levelOne/:levelTwo/:levelThree" element={<Product />} />
+        <Route path="/product/:productId" element={<ProductDetails />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/account/order" element={<Order />} />
+        <Route path="/account/order/:orderId" element={<OrderDetails />} />
+      </Routes>
+      <div className="">
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default CustomerRoutes;
