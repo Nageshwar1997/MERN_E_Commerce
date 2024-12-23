d4910345c0868c3157f67dba88b8b72d921849bb
Nageshwar <nageshpawarpatil@gmail.com>
2024-12-24 02:58:11 +0530

    CartItem Service Created

---
+++ backend/src/services/cartItem.service.js
@@ -0,0 +1,78 @@
+const CartItem = require("../models/cartItem.model");
+const userService = require("../services/user.service");
+
+const updateCartItem = async (userId, cartItemId, cartItemData) => {
+  try {
+    // console.log("userId, cartItemId, cartItemData", userId, cartItemId, cartItemData);
+    const item = await findCartItemById(cartItemId);
+    // console.log("item", item);
+
+    if (!item) {
+      throw new Error("Cart item not found with given id", cartItemId);
+    }
+
+    const user = await userService.findUserById(item.userId);
+    if (!user) {
+      throw new Error("User not found with given id", userId);
+    }
+
+    if (user._id.toString() === userId.toString()) {
+      // if not working then replace it with item.userId.toString() to userId.toString()
+      item.quantity = cartItemData.quantity;
+      item.price = item.product.price * item.quantity;
+      item.discountedPrice = item.product.discountedPrice * item.quantity;
+
+      const updatedCartItem = await item.save();
+      return updatedCartItem;
+    } else {
+      throw new Error("User not authorized to update this cart item");
+    }
+  } catch (error) {
+    throw new Error(error.message || "Failed to update cart item");
+  }
+};
+
+const removeCartItem = async (userId, cartItemId) => {
+  // console.log("userId, cartItemId", userId, cartItemId);
+  try {
+    const cartItem = await findCartItemById(cartItemId);
+
+    //   console.log("cartItem", cartItem);
+    const user = await userService.findUserById(userId);
+    //   console.log("user", user, "cartItemId", cartItemId);
+
+    //   console.log(
+    //     "user._id.toString() === cartItem.userId.toString()",
+    //     user._id.toString() === cartItem.userId.toString()
+    //   );
+
+    if (user._id.toString() === cartItem.userId.toString()) {
+      await CartItem.findByIdAndDelete(cartItemId);
+
+      return "Cart item removed successfully";
+    } else {
+      throw new Error("User not authorized to remove cart item");
+    }
+  } catch (error) {
+    throw new Error(error.message || "Failed to remove cart item");
+  }
+};
+
+const findCartItemById = async (cartItemId) => {
+  try {
+    const cartItem = await CartItem.findById(cartItemId);
+    //   .populate("product");
+
+    // console.log("cartItem", cartItem);
+
+    if (!cartItem) {
+      throw new Error("Cart item not found with given id", cartItemId);
+    }
+
+    return cartItem;
+  } catch (error) {
+    throw new Error(error.message || "Failed to find cart item");
+  }
+};
+
+module.exports = { updateCartItem, removeCartItem, findCartItemById };
