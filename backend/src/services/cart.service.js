f4ca46d249b4626339609ed51d0c94b4b0d21467
Nageshwar <nageshpawarpatil@gmail.com>
2024-12-24 02:57:19 +0530

    Created Cart Service

--- backend/src/services/cart.service.js
+++ backend/src/services/cart.service.js
@@ -1,4 +1,6 @@
 const Cart = require("../models/cart.model");
+const CartItem = require("../models/cartItem.model");
+const Product = require("../models/product.model");
 
 const createCart = async (user) => {
   try {
@@ -15,6 +17,80 @@ const createCart = async (user) => {
   }
 };
 
+const findUserCart = async (userId) => {
+  try {
+    let cart = await Cart.findOne({ user: userId });
+    let cartItems = await CartItem.find({ cart: cart._id }).populate("product");
+
+    cart.cartItems = cartItems;
+    let totalPrice = 0;
+    let totalDiscountedPrice = 0;
+    let totalItem = 0;
+
+    for(let cartItem of cart.cartItems){
+      totalPrice += cartItem.price;
+      totalDiscountedPrice += cartItem.discountedPrice;
+      totalItem += cartItem.quantity;
+    }
+
+
+    cart.totalPrice = totalPrice;
+    cart.totalDiscountedPrice = totalDiscountedPrice;
+    cart.totalItem = totalItem;
+    cart.totalDiscount = totalPrice - totalDiscountedPrice;
+
+    // await cart.save();
+
+    return cart;
+  } catch (error) {
+    throw new Error(error.message || "Failed to find user cart");
+  }
+};
+
+const addCartItem = async (userId, req) => {
+  try {
+    const cart = await Cart.findOne({ user: userId });
+
+    // if (!cart) {
+    //   throw new Error("Cart not found");
+    // }
+
+    const product = await Product.findById(req.productId);
+    // if (!product) {
+    //   throw new Error("Product not found");
+    // }
+
+    const isPresent = await CartItem.findOne({
+      cart: cart._id,
+      product: product._id,
+      userId,
+    });
+
+    if (!isPresent) {
+      const cartItem = new CartItem({
+        product: product._id,
+        cart: cart._id,
+        quantity: 1,
+        userId,
+        price: product.price,
+        size: req.size,
+        discountedPrice: product.discountedPrice,
+      });
+
+      const createdCartItem = await cartItem.save();
+
+      cart.cartItems.push(createdCartItem);
+
+      await cart.save();
+
+      return "Item added to cart";
+    }
+  } catch (error) {
+    throw new Error(error.message || "Failed to add cart item");
+  }
+};
 module.exports = {
   createCart,
+  findUserCart,
+  addCartItem
 };
