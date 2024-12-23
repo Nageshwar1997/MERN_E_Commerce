81300423d7427fa6e2a3b0729b63b1408d4597ff
nageshwarp1997 <nageshwar@ctruh.com>
2024-12-23 22:16:17 +0530

    JWT Provider Done

---
+++ backend/src/config/jwtProvider.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+const generateToken = (userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "2d",
+  });
+  return token;
+};
+
+const getUserIdFromToken = (token) => {
+  const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+  return decodedToken.userId;
+};
+
+module.exports = { generateToken, getUserIdFromToken };
