297a46e478659df7291d0245856da36ce4bc5d9e
nageshwarp1997 <nageshwar@ctruh.com>
2024-12-23 17:07:55 +0530

    Backend started

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,4 +6,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (_, res) => {
+  res.status(200).send("Welcome to E-commerce App Backend");
+});
+
 module.exports = app;
