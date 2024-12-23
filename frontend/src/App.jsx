1aa79a9cf5378892aee7851f5f1a43f73c1c4314
nageshwarp1997 <nageshwar@ctruh.com>
2024-12-23 13:23:27 +0530

    Order Summary section done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,13 +7,15 @@ import Product from "./customer/components/Product/Product";
 import ProductDetails from "./customer/components/ProductDetails/ProductDetails";
 import Cart from "./customer/components/cart/Cart";
 import Checkout from "./customer/components/checkout/Checkout";
+import Order from "./customer/components/order/Order";
+import OrderDetails from "./customer/components/order/OrderDetails";
 
 function App() {
   return (
     <Router>
       <Navigation />
       <Routes>
-        <Route path="/" element={<Checkout />} />
+        <Route path="/" element={<OrderDetails />} />
       </Routes>
       <Footer />
     </Router>
--- frontend/src/customer/components/checkout/OrderSummary.jsx
+++ frontend/src/customer/components/checkout/OrderSummary.jsx
@@ -5,7 +5,62 @@ import CartItem from "../cart/CartItem";
 import { useDispatch, useSelector } from "react-redux";
 // import { getOrderById } from "../../../state/order/action";
 import { useLocation, useNavigate } from "react-router-dom";
+import AddressCard from "../addressCard/AddressCard";
 // import { createPayment } from "../../../state/payment/action";
+
+const cart = {
+  totalPrice: 5000,
+  totalDiscount: 100,
+  totalDiscountedPrice: 4900,
+
+  quantity: 5,
+  size: "M",
+  price: 500,
+  discount: 10,
+  product: {
+    imageUrl:
+      "https://rukminim1.flixcart.com/image/612/612/l5h2xe80/kurta/x/6/n/xl-kast-tile-green-majestic-man-original-imagg4z33hu4kzpv.jpeg?q=70",
+    brand: "Majestic Man",
+    title: "Men Printed Pure Cotton Straight Kurta",
+    color: "Green",
+    discountedPrice: 499,
+    price: 1499,
+    discountPersent: 66,
+    size: [
+      {
+        name: "S",
+        quantity: 20,
+      },
+      {
+        name: "M",
+        quantity: 30,
+      },
+      {
+        name: "L",
+        quantity: 50,
+      },
+    ],
+    quantity: 100,
+    topLavelCategory: "Men",
+    secondLavelCategory: "Clothing",
+    thirdLavelCategory: "mens_kurta",
+    description:
+      "A traditional garment embodying elegance and grace. Crafted from fine fabrics, it features intricate embroidery and a relaxed fit, providing comfort and style.",
+  },
+};
+
+
+  const address = {
+    firstName: "Nageshwar",
+    lastName: "Pawar",
+    address: "Amdura",
+    city: "Nanded",
+    state: "Maharashtra",
+    zip: "431806",
+    country: "India",
+    mobileNumber: "+919503198637",
+    alternateMobileNumber: "+919730870409",
+  };
 
 const OrderSummary = () => {
   const dispatch = useDispatch();
@@ -18,9 +73,9 @@ const OrderSummary = () => {
 
   // console.log("order", order);
 
-  const handleCheckout = ()=>{
+  const handleCheckout = () => {
     // dispatch(createPayment(orderId))
-  }
+  };
 
   // useEffect(() => {
   //   dispatch(getOrderById(orderId));
@@ -29,13 +84,15 @@ const OrderSummary = () => {
     <div>
       <div className="p-5 shadow-lg rounded-s-md border">
         {/* <AddressCard address={order?.shippingAddress} /> */}
+        <AddressCard address={address} />
       </div>
       <div>
         <div className="lg:grid grid-cols-3 my-4 rounded-s-md shadow-lg py-2 relative">
           <div className="col-span-2">
             {/* {order?.orderItems?.map((item) => (
               <CartItem key={item._id} item={item} />
-            ))} */}
+              ))} */}
+            <CartItem item={cart} />
           </div>
           <div className="px-5 sticky top-0 h-[100vh] mt-5 lg:mt-0">
             <div className="border p-4">
@@ -51,20 +108,22 @@ const OrderSummary = () => {
                       {/* ₹ {order?.totalPrice * order?.totalItems} */} 1000
                     </span>
                     <span className="text-green-600">
-                      {/* ₹ {order?.totalDiscountedPrice * order?.totalItems} */} 10
+                      {/* ₹ {order?.totalDiscountedPrice * order?.totalItems} */}{" "}
+                      10
                     </span>
                   </span>
                 </div>
                 <div className="flex justify-between pt-3">
                   <span>Discount</span>
                   <span className="text-green-600">
-                    {/* ₹ -{order?.totalDiscount * order?.totalItems} */}  500
+                    {/* ₹ -{order?.totalDiscount * order?.totalItems} */} 500
                   </span>
                 </div>
                 <div className="flex justify-between pt-3">
                   <span>Delivery Charges</span>
                   <span className="text-green-600">
-                    {/* {order?.totalDiscountedPrice > 499 ? "Free" : "₹ 40"} */} Free
+                    {/* {order?.totalDiscountedPrice > 499 ? "Free" : "₹ 40"} */}{" "}
+                    Free
                   </span>
                 </div>
                 <div className="w-full border-t flex justify-between items-center py-3 font-bold">
@@ -73,7 +132,8 @@ const OrderSummary = () => {
                     ₹{" "}
                     {/* {order?.totalDiscountedPrice * order?.totalItems > 499
                       ? order?.totalDiscountedPrice * order?.totalItems
-                      : order?.totalDiscountedPrice * order?.totalItems + 40} */} 499
+                      : order?.totalDiscountedPrice * order?.totalItems + 40} */}{" "}
+                    499
                   </span>
                 </div>
                 <Button
