996a5bb6badd051c527bef26ca0b2b72b7a1b359
Nageshwar <nageshpawarpatil@gmail.com>
2024-12-25 03:21:55 +0530

    authReducer added

--- frontend/src/state/store.js
+++ frontend/src/state/store.js
@@ -5,17 +5,18 @@ import {
 } from "redux";
 import thunk from "redux-thunk";
 import { authReducer } from "./auth/reducer";
-import { customerProductReducer } from "./product/reducer";
-import { cartReducer } from "./cart/reducer";
-import { orderReducer } from "./order/reducer";
-import { paymentReducer } from "./payment/reducer";
+// import { authReducer } from "./auth/reducer";
+// import { customerProductReducer } from "./product/reducer";
+// import { cartReducer } from "./cart/reducer";
+// import { orderReducer } from "./order/reducer";
+// import { paymentReducer } from "./payment/reducer";
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  products: customerProductReducer,
-  cart: cartReducer,
-  order: orderReducer,
-  payment: paymentReducer,
+  // products: customerProductReducer,
+  // cart: cartReducer,
+  // order: orderReducer,
+  // payment: paymentReducer,
 });
 
 export const store = createStore(rootReducer, applyMiddleware(thunk));
