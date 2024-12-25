9674f67a7df24e5bd0837207154ac6825a5192ed
Nageshwar <nageshpawarpatil@gmail.com>
2024-12-26 00:11:10 +0530

    nothing

--- frontend/src/state/product/action.js
+++ frontend/src/state/product/action.js
@@ -28,19 +28,15 @@ export const findProducts = (reqData) => async (dispatch) => {
       `/api/products?color=${color}&sizes=${sizes}&minPrice=${minPrice}&maxPrice=${maxPrice}&minDiscount=${minDiscount}&category=${category}&stock=${stock}&sort=${sort}&pageNumber=${pageNumber}&pageSize=${pageSize}`
     );
 
-    console.log("products", data);
-
     dispatch({ type: FIND_PRODUCTS_SUCCESS, payload: data });
   } catch (error) {
     dispatch({ type: FIND_PRODUCTS_FAILURE, payload: error.message });
   }
 };
 
-export const findProductById = (reqData) => async (dispatch) => {
+export const findProductById = (productId) => async (dispatch) => {
   dispatch({ type: FIND_PRODUCT_BY_ID_REQUEST });
   try {
-    const productId = reqData;
-
     const { data } = await api.get(`/api/products/id/${productId}`);
     // console.log("data", data);
 
